a4c309df571eb9f40d03990f40a04519104734f6
shinyZu <shinysirichandra1141@gmail.com>
2022-11-20 13:53:26 +0530

    Practice Spring Boot basics

---
+++ 41_actuator-demo-03/src/main/java/com/ceyentra/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -0,0 +1,29 @@
+package com.ceyentra.springboot.demo.mycoolapp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@RestController
+public class FunRestController {
+
+    // expose "/" that return "Hello World"
+    @GetMapping("/")
+    public String sayHello(){
+        return "Hello World!!!!!! Time on server is " + LocalDateTime.now();
+    }
+
+    // expose a new endpoint for "workout"
+    @GetMapping("/workout")
+    public String getDailyWorkout(){
+        return "Run a hard 5K!!";
+    }
+
+    // expose a new endpoint for "fortune"
+    @GetMapping("/fortune")
+    public String getDailyFortune(){
+        return "Today is your lucky day!!";
+    }
+
+}
