354f7de12deee894639b6bd3d9e559a546eb9d7a
shinyZu <shinysirichandra1141@gmail.com>
2022-11-20 09:51:12 +0530

    Practice Spring REST API

---
+++ 38_spring-crm-rest-demo/src/main/java/com/ceyentra/springdemo/dao/CustomerDAO.java
@@ -0,0 +1,17 @@
+package com.ceyentra.springdemo.dao;
+
+import java.util.List;
+
+import com.ceyentra.springdemo.entity.Customer;
+
+public interface CustomerDAO {
+
+	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer theCustomer);
+
+	public Customer getCustomer(int theId);
+
+	public void deleteCustomer(int theId);
+	
+}
